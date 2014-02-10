3ddf85a0b77381140105da8deee9199828424385
Austin Bruch <brucha220@gmail.com>
2014-02-10 16:29:05 -0500

    Expandable List View fix

--- src/com/lectureloot/android/ScheduleFragment.java
+++ src/com/lectureloot/android/ScheduleFragment.java
@@ -1,32 +1,23 @@
 package com.lectureloot.android;
 
-import com.lectureloot.android.R;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import android.app.Activity;
 import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.ExpandableListView.OnGroupCollapseListener;
-import android.widget.ExpandableListView.OnGroupExpandListener;
-import android.widget.Toast;
 
 
 public class ScheduleFragment extends Fragment {
 	
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,42 +29,40 @@ public class ScheduleFragment extends Fragment {
        expListView = (ExpandableListView) rootView.findViewById(R.id.lvExp);
 // 
 //        // preparing list data
-//        prepareListData();
+        prepareListData();
 // 
-        listAdapter = new ExpandableListAdapter(rootView.getContext(), listDataHeader, listDataChild);
+        listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
 // 
 //        // setting list adapter
         expListView.setAdapter(listAdapter);
 		
 		return rootView;
 	}
-
 	
-//    private void prepareListData() {
-//        listDataHeader = new ArrayList<String>();
-//        listDataChild = new HashMap<String, List<String>>();
-// 
-//        // Adding child data
-//        listDataHeader.add("CEN3031");
-//        listDataHeader.add("CAP4053");
-//        listDataHeader.add("EGN4641");
-// 
-//        // Adding child data
-//        List<String> cen3031 = new ArrayList<String>();
-//        cen3031.add("Dummy Data");
-//
-// 
-//        List<String> cap4053 = new ArrayList<String>();
-//        cap4053.add("Dummy Data");
-//
-// 
-//        List<String> egn4641 = new ArrayList<String>();
-//        egn4641.add("Dummy Data");
-//
-// 
-//        listDataChild.put(listDataHeader.get(0), cen3031); // Header, Child data
-//        listDataChild.put(listDataHeader.get(1), cap4053);
-//        listDataChild.put(listDataHeader.get(2), egn4641);
-//    }
-//	
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+ 
+        // Adding child data
+        listDataHeader.add("CEN3031");
+        listDataHeader.add("CAP4053");
+        listDataHeader.add("EGN4641");
+ 
+        // Adding child data
+        List<String> cen3031 = new ArrayList<String>();
+        cen3031.add("Dummy Data");
+
+ 
+        List<String> cap4053 = new ArrayList<String>();
+        cap4053.add("Dummy Data");
+
+ 
+        List<String> egn4641 = new ArrayList<String>();
+        egn4641.add("Dummy Data");
+
+ 
+        listDataChild.put(listDataHeader.get(0), cen3031); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), cap4053);
+        listDataChild.put(listDataHeader.get(2), egn4641);
+    }
 }
