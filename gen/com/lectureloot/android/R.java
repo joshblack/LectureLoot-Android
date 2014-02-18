6972ca89defd9538409d1dc420eda63809da1c96
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2014-02-17 19:19:22 -0500

    adding stuff

--- gen/com/lectureloot/android/R.java
+++ gen/com/lectureloot/android/R.java
@@ -22,10 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int profile_pic=0x7f020001;
-        public static final int progress_triangle=0x7f020002;
-        public static final int red_green_gradient=0x7f020003;
-        public static final int red_green_gradient_transparent=0x7f020004;
+        public static final int lecture_loot_logo=0x7f020001;
+        public static final int lectureloot_launcher=0x7f020002;
+        public static final int profile_pic=0x7f020003;
+        public static final int progress_triangle=0x7f020004;
+        public static final int red_green_gradient=0x7f020005;
+        public static final int red_green_gradient_transparent=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f08000b;
