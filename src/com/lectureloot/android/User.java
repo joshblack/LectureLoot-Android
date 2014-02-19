fe551d1c15add78a53b5fbcb2de22a65cfbf0012
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2014-02-19 12:30:03 -0500

    created User class

---
+++ src/com/lectureloot/android/User.java
@@ -0,0 +1,14 @@
+package com.lectureloot.android;
+
+public class User {
+	private String mName;
+	private int mPoints;
+	//private ArrayList<Wager> mWagers;
+	//private ArrayList<Course> mCourses;
+	//private ArrayList<Meetings> mMeetings;
+	
+	public User(String name){
+		mName = name;
+	}
+
+}
