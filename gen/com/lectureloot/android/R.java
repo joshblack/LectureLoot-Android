c6235da512b3887b953b4136b7758c14b2311fbe
Dgilliams <uffoward@gmail.com>
2014-02-21 19:41:02 -0500

    Switching to Master to Pull Updates

--- gen/com/lectureloot/android/R.java
+++ gen/com/lectureloot/android/R.java
@@ -69,28 +69,58 @@ public final class R {
         public static final int user_circle=0x7f02002d;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int check_in_button=0x7f080002;
-        public static final int create_wager=0x7f08000b;
-        public static final int next_class_display=0x7f080001;
+        public static final int action_settings=0x7f080025;
+        public static final int addButton=0x7f080023;
+        public static final int addCourse=0x7f080001;
+        public static final int check_in_button=0x7f080004;
+        public static final int courseCode=0x7f080005;
+        public static final int courseCodeSpinner=0x7f080014;
+        public static final int courseTitle=0x7f080008;
+        public static final int create_wager=0x7f080024;
+        public static final int credits=0x7f080007;
+        public static final int deptCodeSpinner=0x7f080002;
+        public static final int dialogAddButton=0x7f080016;
+        public static final int dropButton=0x7f080013;
+        public static final int instructor=0x7f080009;
+        public static final int lvExp=0x7f080022;
+        public static final int mapButton=0x7f080012;
+        public static final int meetingDays1=0x7f08000c;
+        public static final int meetingDays2=0x7f08000e;
+        public static final int meetingDays3=0x7f080010;
+        public static final int meetingDaysLabel=0x7f08000a;
+        public static final int nextMeeting=0x7f08001f;
+        public static final int next_class_display=0x7f080003;
         public static final int pager=0x7f080000;
-        public static final int user_lectures_remaining_notice=0x7f08000a;
-        public static final int user_lectures_remaining_notice_variable=0x7f080009;
-        public static final int user_points=0x7f080004;
-        public static final int user_profile_pic=0x7f080003;
-        public static final int user_statistics=0x7f080005;
-        public static final int user_streak1=0x7f080006;
-        public static final int user_streak2=0x7f080008;
-        public static final int user_streak_variable=0x7f080007;
+        public static final int room1=0x7f08000d;
+        public static final int room2=0x7f08000f;
+        public static final int room3=0x7f080011;
+        public static final int roomLabel=0x7f08000b;
+        public static final int sectionNumber=0x7f080006;
+        public static final int sectionNumberSpinner=0x7f080015;
+        public static final int timeLeft=0x7f080020;
+        public static final int user_lectures_remaining_notice=0x7f08001e;
+        public static final int user_lectures_remaining_notice_variable=0x7f08001d;
+        public static final int user_points=0x7f080018;
+        public static final int user_profile_pic=0x7f080017;
+        public static final int user_statistics=0x7f080019;
+        public static final int user_streak1=0x7f08001a;
+        public static final int user_streak2=0x7f08001c;
+        public static final int user_streak_variable=0x7f08001b;
+        public static final int username=0x7f080021;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int check_in_box=0x7f030001;
-        public static final int display_box=0x7f030002;
-        public static final int fragment_dashboard=0x7f030003;
-        public static final int fragment_schedule=0x7f030004;
-        public static final int fragment_wager=0x7f030005;
-        public static final int test=0x7f030006;
+        public static final int add_new_course_group=0x7f030001;
+        public static final int add_new_course_list_item=0x7f030002;
+        public static final int check_in_box=0x7f030003;
+        public static final int course_list_group=0x7f030004;
+        public static final int course_list_item=0x7f030005;
+        public static final int dialog_add_course=0x7f030006;
+        public static final int display_box=0x7f030007;
+        public static final int fragment_dashboard=0x7f030008;
+        public static final int fragment_schedule=0x7f030009;
+        public static final int fragment_wager=0x7f03000a;
+        public static final int test=0x7f03000b;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -98,18 +128,19 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int check_in_button=0x7f050007;
-        public static final int course_code=0x7f050009;
-        public static final int create_wager=0x7f050006;
+        public static final int check_in_button=0x7f050008;
+        public static final int course_code=0x7f05000a;
+        public static final int create_wager=0x7f050007;
         public static final int dashboard_title=0x7f050003;
+        public static final int department_code=0x7f050005;
         public static final int schedule_title=0x7f050002;
-        public static final int time_before_class=0x7f05000a;
-        public static final int user_lectures_remaining_notice=0x7f05000d;
-        public static final int user_name=0x7f050005;
-        public static final int user_statistics=0x7f05000e;
-        public static final int user_streak1=0x7f05000b;
-        public static final int user_streak2=0x7f05000c;
-        public static final int user_total_points=0x7f050008;
+        public static final int time_before_class=0x7f05000b;
+        public static final int user_lectures_remaining_notice=0x7f05000e;
+        public static final int user_name=0x7f050006;
+        public static final int user_statistics=0x7f05000f;
+        public static final int user_streak1=0x7f05000c;
+        public static final int user_streak2=0x7f05000d;
+        public static final int user_total_points=0x7f050009;
         public static final int wager_title=0x7f050004;
     }
     public static final class style {
