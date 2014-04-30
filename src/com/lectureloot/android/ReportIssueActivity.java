1fd5b5fe8f06ce05ffb1f4585a6408c237e09e11
Austin Bruch <brucha220@gmail.com>
2014-04-30 16:39:24 -0400

    Notifications and Report an Issue Stub Functionality Added

--- src/com/lectureloot/android/ReportIssueActivity.java
+++ src/com/lectureloot/android/ReportIssueActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewDebug.FlagToString;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
