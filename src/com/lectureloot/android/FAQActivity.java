8385c0e8f0d87ee6628bcfdc2211e840bdbdca7c
Austin Bruch <brucha220@gmail.com>
2014-05-01 13:04:22 -0400

    Updated Frequently Asked Questions Content

--- src/com/lectureloot/android/FAQActivity.java
+++ src/com/lectureloot/android/FAQActivity.java
@@ -1,6 +1,10 @@
 package com.lectureloot.android;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -8,11 +12,28 @@ public class FAQActivity extends Activity {
 	private TextView faqTextView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_faq);
 
 		faqTextView = (TextView) findViewById(R.id.faq_text_view);
-		faqTextView.setText("Frequently Asked Questions: \n1. Question 1 \n2. Question 2 \n3. Question 3 \n4. Question 4"); 
+		String faq="";
+
+		AssetManager assetManager = getResources().getAssets();
+		InputStream inputStream = null;
+
+		try {
+			inputStream = assetManager.open("faq.txt");
+			if ( inputStream != null) {
+				java.util.Scanner s = new java.util.Scanner(inputStream).useDelimiter("\\A");
+				while(s.hasNext())
+				{
+					faq+=(s.next());
+					faq+=("\n");
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		faqTextView.setText(faq);
 	}
 }
